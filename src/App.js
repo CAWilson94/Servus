18850068f23ef10c113a1b01f063be31afcddf88
CAWilson94 <charlotteawilson.94@gmail.com>
2018-05-06 17:03:50 +0100

    s

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ServusIO</h1>
+          <h1 className="App-title">Servus IO</h1>
         </header>
         <p>Boopity!</p>
       </div>
