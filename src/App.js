eec7fd80ff784b98e4da9bee171b4b866c2adc3f
CAWilson94 <charlotteawilson.94@gmail.com>
2018-05-07 12:43:10 +0100

    fucking around

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import logo from './favicon.ico';
+import webdev from './web.jpg';
 import './App.css';
 import WordCloud from 'react-d3-cloud';
+
 
 const strings = ["He doesn't look like he is from here", "She left it a bit late to start running", "mutants"]
 const data = [
   { text:  strings[0], value: strings[0].length },
   { text:  strings[1], value: strings[1].length },
   { text:  strings[2], value: strings[2].length },
+];
+
+const strings_1 = ["Beans on toast", "Fish and Chips", "boop"]
+const data_1 = [
+  { text:  strings_1[0], value: strings_1[0].length },
+  { text:  strings_1[1], value: strings_1[1].length },
+  { text:  strings_1[2], value: strings_1[2].length },
 ];
  
 const fontSizeMapper = word => Math.log2(word.value) * 5;
@@ -23,9 +32,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Servus IO</h1>
-        </header>    
+        </header> 
+        <div className="photos">
+        <img src={webdev} className="webdev-photo" alt="webdev"/>
+        </div>          
         <div className="words">    
-        <WordCloud data={data} font={font} fontSizeMapper={fontSizeMapper}/>
+        <WordCloud data={data_1} font={font} fontSizeMapper={fontSizeMapper}/>
         </div>
       </div>
     );
---
+++ src/EasyBarChart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class EasyBarChart extends Component {  
+  render() {
+    return (
+      <div className="Easy">          
+      </div>
+    );
+  }
+}
+
+export default BarRac;
